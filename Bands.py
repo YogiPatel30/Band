eddc57eb700db82e83bf8771ac21b22ae34551b5
BillyBobjrjr22 <95716758+BillyBobjrjr22@users.noreply.github.com>
2021-12-07 14:59:58 -0500

    Add files via upload

---
+++ Bands.py
@@ -0,0 +1,20 @@
+##import random
+##def Bruces(codecheck):
+##    
+###main program
+##value=[]
+##listfirst=[1,2,3,4,5,6,7,8,9]
+##listrest=[1,2,3,4,5,6,7,8,9,0]
+##first=random.choice(listfirst)
+##value.append(first)
+##listrest.remove(first)
+##n=0
+##for n1 in range(0,3):
+##    rest=random.choice(listrest)
+##    value.append(rest)
+##    listrest.remove(rest)
+##code=int(str(value[0])+str(value[1])+str(value[2])+str(value[3]))
+##print(code)
+##
+
+ans=int(input("Please choose a 4 digit integer: "))
